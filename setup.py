2ca25f1aaf1fdf44f9032ddeeded6cb886375f5f
Psylocibus <120992315+Psylocibus@users.noreply.github.com>
2026-01-16 12:42:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'django-appconf',
-        'https://github.com/Psylocibus/pilkit.git@psylocibus_custom_version',
+        'git+https://github.com/Psylocibus/pilkit.git@psylocibus_custom_version',
     ],
     extras_require={
         'async': ['django-celery>=3.0'],
