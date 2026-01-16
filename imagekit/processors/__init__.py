45c3bb04d586048a31b9fd373f759e9fcaa8564b
Psylocibus <psylocibus@mailo.com>
2026-01-16 12:19:58 +0100

    import processors from Psylocibus:pilkit - custom_version_psylocibus

--- imagekit/processors/__init__.py
+++ imagekit/processors/__init__.py
@@ -3,9 +3,13 @@ from pilkit.processors import *
 __all__ = [
     # Base
     'ProcessorPipeline', 'Adjust', 'Reflection', 'Transpose',
-    'Anchor', 'MakeOpaque',
+    'Anchor', 'MakeOpaque', 'SetOpacity'
     # Crop
     'TrimBorderColor', 'Crop', 'SmartCrop',
+    # Filter
+    'GaussianBlur'
+    # Overlay
+    'ColorOverlay', 'ImageOverlay'
     # Resize
     'Resize', 'ResizeToCover', 'ResizeToFill', 'SmartResize',
     'ResizeCanvas', 'AddBorder', 'ResizeToFit', 'Thumbnail'
